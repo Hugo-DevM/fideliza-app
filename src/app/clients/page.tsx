ef0b8629d78b89a2d6a4e5dc490ce7f2541eebf7
Hugo Montaño <mobile.developer.mx@gmail.com>
2025-11-23 18:05:23 -0600

    Add new page components for Clients, Reports, Rewards, and Settings with corresponding CSS styles for layout management

---
+++ src/app/clients/page.tsx
@@ -0,0 +1,5 @@
+import styles from "./page.module.css";
+
+export default function Clients() {
+  return <div className={styles.page}>Clientes</div>;
+}
---
+++ src/app/rewards/page.tsx
@@ -0,0 +1,5 @@
+import styles from "./page.module.css";
+
+export default function Rewards() {
+  return <div className={styles.page}>Rewards</div>;
+}
