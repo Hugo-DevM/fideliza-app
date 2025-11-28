8b67f98ced85148e028f24818d30ac38e58ea54c
Hugo Montaño <mobile.developer.mx@gmail.com>
2025-11-27 20:54:01 -0600

    Update session management in Supabase middleware to include additional route checks for registration and email confirmation pages

--- src/lib/supabase/middleware.ts
+++ src/lib/supabase/middleware.ts
@@ -35,7 +35,8 @@ export async function updateSession(request: NextRequest) {
   if (
     !user &&
     !request.nextUrl.pathname.startsWith("/login") &&
-    !request.nextUrl.pathname.startsWith("/auth")
+    !request.nextUrl.pathname.startsWith("/register") &&
+    !request.nextUrl.pathname.startsWith("/emailconfirm")
   ) {
     const url = request.nextUrl.clone();
     url.pathname = "/login";
