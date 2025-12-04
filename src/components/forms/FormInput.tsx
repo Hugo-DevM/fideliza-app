05f7bca0af3f2e39192a13394e258e07be9b6f61
Hugo Montaño <mobile.developer.mx@gmail.com>
2025-12-03 18:26:13 -0600

    Refactor FormInput component to simplify props handling
    - Updated FormInput to directly spread props, reducing boilerplate code and improving readability.

--- src/components/forms/FormInput.tsx
+++ src/components/forms/FormInput.tsx
@@ -4,20 +4,8 @@ import styles from "./FormInput.module.css";
 type InputCustomProps = React.InputHTMLAttributes<HTMLInputElement>;
 
 const FormInput = React.forwardRef<HTMLInputElement, InputCustomProps>(
-  ({ type, placeholder, id, name, onChange, onBlur, ...rest }, ref) => {
-    return (
-      <input
-        ref={ref}
-        id={id}
-        name={name}
-        type={type}
-        placeholder={placeholder}
-        onChange={onChange}
-        onBlur={onBlur}
-        {...rest}
-        className={styles.inputCustom}
-      />
-    );
+  (props, ref) => {
+    return <input ref={ref} className={styles.inputCustom} {...props} />;
   }
 );
 
